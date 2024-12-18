47cee316af0bc674cfa7c926a10322973ab42df1
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2024-12-18 13:48:27 +0530

    adding color

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
+import { useRouter } from "expo-router";
 
 const index = () => {
+  const router = useRouter();
   return (
     <View>
       <Text>Index</Text>
+      <Button title="welcome" onPress={() => router.push("/welcome")}></Button>
     </View>
   );
 };
--- components/ScreenWrapper.tsx
+++ components/ScreenWrapper.tsx
@@ -1,20 +1,17 @@
 import { View, Text } from "react-native";
 
-import React from "react";
+import React, { ReactNode } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-
 interface ScreenWrapperProps {
-  children: any;
-  bg: any;
+  children: ReactNode;
+  bg: string;
 }
 
 const ScreenWrapper = ({ children, bg }: ScreenWrapperProps) => {
   const { top } = useSafeAreaInsets();
   const paddingTop = top > 0 ? top + 5 : 30;
   return (
-    <View>
-      <Text>ScreenWrapper</Text>
-    </View>
+    <View style={{ flex: 1, paddingTop, backgroundColor: bg }}>{children}</View>
   );
 };
 
