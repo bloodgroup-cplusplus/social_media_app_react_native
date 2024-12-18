93962229dd1ca95d546900aebf39aadadd19724c
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2024-12-18 20:13:35 +0530

    adding icons

--- app/login.tsx
+++ app/login.tsx
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Home from "../assets/icons/Home";
+import { theme } from "@/constants/theme";
 
 const Login: React.FC = () => {
   return (
-    <View>
+    <ScreenWrapper bg={"white"}>
       <Text>Login</Text>
-    </View>
+      <Home strokeWidth={2} color={theme.colors.rose} />
+    </ScreenWrapper>
   );
 };
 export default Login;
