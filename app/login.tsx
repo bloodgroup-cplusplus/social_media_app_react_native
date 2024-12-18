60b1b7182da5fdcad2153408158cca0461130702
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2024-12-18 21:43:35 +0530

    adding new componnet

--- app/login.tsx
+++ app/login.tsx
@@ -3,13 +3,21 @@ import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import { theme } from "@/constants/theme";
 import Icon from "../assets/icons";
+import { StatusBar } from "expo-status-bar";
+import BackButton from "@/components/BackButton";
+import { useRouter } from "expo-router";
 
 const Login: React.FC = () => {
+  const router = useRouter();
   return (
     <ScreenWrapper bg={"white"}>
-      <Text>Login</Text>
-      <Icon name="home" color="red" />
+      <StatusBar style="dark" />
+      <View>
+        <BackButton router={router} size={26} />
+      </View>
     </ScreenWrapper>
   );
 };
 export default Login;
+
+//const styles = StyleSheet.create{}
--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Icon from "@/assets/icons";
+import { theme } from "@/constants/theme";
+import { Router } from "expo-router";
+interface BackButtonProps {
+  size: number;
+  router: Router;
+}
 
-const BackButton = () => {
+const BackButton = ({ size, router }: BackButtonProps) => {
   return (
-    <View>
-      <Text>Back Button</Text>
-    </View>
+    <Pressable>
+      <Icon
+        name="arrowLeft"
+        strokeWidth={2.5}
+        size={size}
+        color={theme.colors.text}
+      />
+    </Pressable>
   );
 };
 
 export default BackButton;
+
+const styles = StyleSheet.create({
+  button: {
+    alignSelf: "flex-start",
+    padding: 5,
+    borderRadius: theme.radius.sm,
+    backgroundColor: "rgba(0,0,0,0,0.07)",
+  },
+});
