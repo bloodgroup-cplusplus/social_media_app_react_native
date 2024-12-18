79d6d0cf2e1a6c39addd897562ff1655679e78e8
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2024-12-18 14:25:09 +0530

    adding horizontal padding

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,15 +1,26 @@
 import { StyleSheet, View, Text } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import { StatusBar } from "expo-status-bar";
+import { width_percentage } from "@/helpers/common";
 
 const Welcome = () => {
   return (
-    <ScreenWrapper bg={"white"}>
-      <Text>Welcome</Text>
+    <ScreenWrapper bg="white">
+      <StatusBar style="dark" />
+      <View style={styles.container}></View>
     </ScreenWrapper>
   );
 };
 
 export default Welcome;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "red",
+    marginHorizontal: width_percentage(10),
+  },
+});
--- helpers/common.ts
+++ helpers/common.ts
@@ -2,10 +2,10 @@ import { Dimensions } from "react-native";
 
 const { width: deviceWidth, height: deviceHeight } = Dimensions.get("window");
 
-const height_percentage = (percentage: number) => {
+export const height_percentage = (percentage: number) => {
   return (percentage * deviceHeight) / 100;
 };
 
-const width_percentage = (percentage: number) => {
+export const width_percentage = (percentage: number) => {
   return (percentage * deviceWidth) / 100;
 };
