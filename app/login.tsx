2cbf725cbdd3ed314808913d93d5da031ba8ed0a
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2024-12-18 20:21:50 +0530

    adding back button components

--- app/login.tsx
+++ app/login.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
-import Home from "../assets/icons/Home";
 import { theme } from "@/constants/theme";
+import Icon from "../assets/icons";
 
 const Login: React.FC = () => {
   return (
     <ScreenWrapper bg={"white"}>
       <Text>Login</Text>
-      <Home strokeWidth={2} color={theme.colors.rose} />
+      <Icon name="home" color="red" />
     </ScreenWrapper>
   );
 };
---
+++ components/BackButton.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const BackButton = () => {
+  return (
+    <View>
+      <Text>Back Button</Text>
+    </View>
+  );
+};
+
+export default BackButton;
